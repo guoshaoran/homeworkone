0970c58d64785bafba0df16f22e6b58308efad16
guo shaoran <2043035530@qq.com>
2024-04-09 08:31:04 +0000

    1

--- hello_console/bin/hello_console.dart
+++ hello_console/bin/hello_console.dart
@@ -1,32 +1,5 @@
-import 'package:console_bars/console_bars.dart';
 import 'package:hello_console/hello_console.dart' as hello_console;
-void main(List<String> args) async {
-  // A bar that looks like
-  //
-  // Loading : ████████████████████████████████████████.................... 673/1000 67.3% [ 0:00:13.28 / 0:00:06.45 ]
-  //
-  final p = FillingBar(
-      desc: "Loading", total: 1000, time: true, percentage: true, scale: 0.2);
-  for (var i = 0; i < 500; i++) {
-    p.increment();
-    await Future.delayed(Duration(milliseconds: 10));
-  }
 
-  // Change the description and total mid-way
-  await Future.delayed(Duration(seconds: 2));
-  p.total = 750;
-  p.desc = "Processing";
-  await Future.delayed(Duration(seconds: 2));
-
-  for (var i = 0; i < 250; i++) {
-    p.increment();
-    await Future.delayed(Duration(milliseconds: 5));
-  }
-  
- print('\nHello world: ${hello_console.calculate()}!');
-
-
-  print("\nDone");
-
-
+void main(List<String> arguments) {
+ print('Hello world: ${hello_console.calculate()}!');
 }
